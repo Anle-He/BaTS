a2b5148f6ddc4cc8550b77dc46271fd6ac6d52b9
Anle-He <sicheng_he@163.com>
2024-11-28 21:07:12 +0800

    [Pipeline] modify runners/LTSFRunner.py

--- runners/LTSFRunner.py
+++ runners/LTSFRunner.py
@@ -1,1 +1,18 @@
 from .AbstractRunner import AbstractRunner
+
+
+class LTSFRunner(AbstractRunner):
+    def __init__(
+            self,
+            cfg: dict,
+            device,
+            scaler,
+            log=None):
+        super().__init__()
+
+        self.cfg = cfg
+        self.device = device
+        self.scaler = scaler
+        self.log = log
+
+        self.clip_grad = cfg['OPTIM'].get('clip_grad')
