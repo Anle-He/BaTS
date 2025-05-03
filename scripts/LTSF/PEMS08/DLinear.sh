9fa773f048c4c2a80c1fc033ed6670081380155f
Anle-He <sicheng_he@163.com>
2025-05-03 19:49:28 +0800

    feat: add new configuration files for DLinear model with varying output steps (24, 48, 96) and update DLinear.sh script for training

--- scripts/LTSF/PEMS08/DLinear.sh
+++ scripts/LTSF/PEMS08/DLinear.sh
@@ -18,3 +18,66 @@ python -u exp_main.py \
     -d PEMS08 \
     -cfg baselines/DLinear/LTSFConfig/PEMS08_IN96_OUT12.yaml \
     -sd 2026
+
+python -u exp_main.py \
+    -m DLinear \
+    -t LTSF \
+    -d PEMS08 \
+    -cfg baselines/DLinear/LTSFConfig/PEMS08_IN96_OUT24.yaml \
+    -sd 2024
+
+python -u exp_main.py \
+    -m DLinear \
+    -t LTSF \
+    -d PEMS08 \
+    -cfg baselines/DLinear/LTSFConfig/PEMS08_IN96_OUT24.yaml \
+    -sd 2025
+
+python -u exp_main.py \
+    -m DLinear \
+    -t LTSF \
+    -d PEMS08 \
+    -cfg baselines/DLinear/LTSFConfig/PEMS08_IN96_OUT24.yaml \
+    -sd 2026
+
+python -u exp_main.py \
+    -m DLinear \
+    -t LTSF \
+    -d PEMS08 \
+    -cfg baselines/DLinear/LTSFConfig/PEMS08_IN96_OUT48.yaml \
+    -sd 2024
+
+python -u exp_main.py \
+    -m DLinear \
+    -t LTSF \
+    -d PEMS08 \
+    -cfg baselines/DLinear/LTSFConfig/PEMS08_IN96_OUT48.yaml \
+    -sd 2025
+
+python -u exp_main.py \
+    -m DLinear \
+    -t LTSF \
+    -d PEMS08 \
+    -cfg baselines/DLinear/LTSFConfig/PEMS08_IN96_OUT48.yaml \
+    -sd 2026
+
+python -u exp_main.py \
+    -m DLinear \
+    -t LTSF \
+    -d PEMS08 \
+    -cfg baselines/DLinear/LTSFConfig/PEMS08_IN96_OUT96.yaml \
+    -sd 2024
+
+python -u exp_main.py \
+    -m DLinear \
+    -t LTSF \
+    -d PEMS08 \
+    -cfg baselines/DLinear/LTSFConfig/PEMS08_IN96_OUT96.yaml \
+    -sd 2025
+
+python -u exp_main.py \
+    -m DLinear \
+    -t LTSF \
+    -d PEMS08 \
+    -cfg baselines/DLinear/LTSFConfig/PEMS08_IN96_OUT96.yaml \
+    -sd 2026
